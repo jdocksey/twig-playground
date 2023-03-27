06b08d9096ef5a4dd5a72ae357d401aeac23757c
Joseph Docksey <joecode@tuta.io>
2023-03-27 10:12:17 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Twig Playground
+
+A Twig playground that runs on [Deno](https://deno.land/), using [Twig.js](https://github.com/twigjs/twig.js/), with
+[Tailwind](https://tailwindcss.com/) integration.
+
+You may find the Visual Studio Code
+[HTML Preview](https://marketplace.visualstudio.com/items?itemName=george-alisson.html-preview-vscode) extension
+helpful.
+
+## To use:
+
+- Remove the `*.example` extension from the `./input/*.example` files
+- Execute `deno task start`
+- Open `./output.html`
+
+The HTML file will be updated as you make changes to the Twig and document templates, the template data, and the
+Tailwind configuration.
---
+++ main.ts
@@ -0,0 +1,74 @@
+import { debounce } from 'https://deno.land/std@0.177.0/async/debounce.ts';
+import Twig from 'https://esm.sh/twig@1.16.0';
+
+const Config = {
+    data: './input/data.json',
+    twigTemplate: './input/template.twig',
+    documentTemplate: './input/document-template.html',
+    tailwindConfig: './input/tailwind.config.js',
+    output: './output.html',
+} as const;
+
+run();
+
+async function run(): Promise<void> {
+    render();
+
+    const debouncedRender = debounce(() => {
+        render();
+    }, 200);
+
+    const watcher = Deno.watchFs([
+        Config.data,
+        Config.twigTemplate,
+        Config.documentTemplate,
+        Config.tailwindConfig,
+    ]);
+    for await (const event of watcher) {
+        if (event.kind === 'modify') {
+            debouncedRender();
+        }
+    }
+}
+
+function render(): void {
+    const data = getData();
+    const documentTemplate = readTextFile(Config.documentTemplate);
+    const twigTemplate = readTextFile(Config.twigTemplate);
+    const tailwindConfig = readTextFile(Config.tailwindConfig);
+
+    const templateObject = Twig.twig({ data: twigTemplate });
+    const renderedHtml = templateObject.render(data);
+    const output = documentTemplate
+        .replace('{{ tailwindConfig }}', tailwindConfig)
+        .replace('{{ contents }}', renderedHtml);
+
+    Deno.writeTextFile(Config.output, output);
+
+    console.log(`updated at ${new Date().toLocaleTimeString()}`);
+}
+
+// deno-lint-ignore no-explicit-any
+function getData(): any {
+    try {
+        const fileText = Deno.readTextFileSync(Config.data);
+        return JSON.parse(fileText);
+    } catch (error) {
+        printErrorMessage(`failed to parse ${Config.data}:`, error);
+        return {};
+    }
+}
+
+function readTextFile(filepath: string): string {
+    try {
+        return Deno.readTextFileSync(filepath);
+    } catch (error) {
+        printErrorMessage('failed to read ${filepath}:', error);
+        return '';
+    }
+}
+
+// deno-lint-ignore no-explicit-any
+function printErrorMessage(message: string, error: any): void {
+    console.log(`\nError: ${message}\n\n${error}`);
+}
