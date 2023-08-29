3c09a94cb27a1e6095084375aed4ac07ac528cf3
Joseph Docksey <joecode@tuta.io>
2023-08-29 11:58:09 -0500

    improve bouncing

--- input/tailwind.config.js
+++ input/tailwind.config.js
@@ -4,7 +4,20 @@
 // https://tailwindcss.com/docs/configuration
 tailwind.config = {
   theme: {
-    extend: {
+    animation: {
+      'bounce': 'bounce .8s infinite'
+    },
+    keyframes: {
+      'bounce': {
+        '0%': {
+          transform: 'translateY(0)',
+          'animation-timing-function': 'cubic-bezier(0, 0, 0 .2, 1)',
+        },
+        '50%': {
+          transform: 'translateY(-20%)',
+          'animation-timing-function': 'cubic-bezier(0.95, 0, 1, 1)',
+        },
+      },
     },
   },
 };
