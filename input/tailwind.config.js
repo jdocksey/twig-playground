e916a39cce69c28a92ea8f48a97c5bc6ac91f926
Joseph Docksey <joecode@tuta.io>
2023-08-29 14:49:36 -0500

    demo

--- input/tailwind.config.js
+++ input/tailwind.config.js
@@ -8,22 +8,6 @@ tailwind.config = {
       'md': '769px',
       'lg': '992px',
     },
-    animation: {
-      // TODO: Add an explanation
-      'bounce-by-growing': 'bounce-by-growing .8s infinite'
-    },
-    keyframes: {
-      'bounce-by-growing': {
-        '0%, 100%': {
-          height: '4rem',
-          'animation-timing-function': 'cubic-bezier(0, 0, 0 .2, 1)',
-        },
-        '50%': {
-          height: '4.8rem',
-          'animation-timing-function': 'cubic-bezier(0.95, 0, 1, 1)',
-        },
-      },
-    },
     extend: {
       boxShadow: {
         'md-heavy': '0 3px 5px 1px rgb(0 0 0 / 0.3)',
