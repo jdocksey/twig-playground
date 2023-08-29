982558af312a517190e7b5ce65767b5ae9eb6d90
Joseph Docksey <joecode@tuta.io>
2023-08-29 12:15:05 -0500

    map4

--- input/tailwind.config.js
+++ input/tailwind.config.js
@@ -4,7 +4,21 @@
 // https://tailwindcss.com/docs/configuration
 tailwind.config = {
   theme: {
-    extend: {
+    animation: {
+      // TODO: Add an explanation
+      'bounce-by-growing': 'bounce-by-growing .8s infinite'
+    },
+    keyframes: {
+      'bounce-by-growing': {
+        '0%, 100%': {
+          height: '4rem',
+          'animation-timing-function': 'cubic-bezier(0, 0, 0 .2, 1)',
+        },
+        '50%': {
+          height: '4.8rem',
+          'animation-timing-function': 'cubic-bezier(0.95, 0, 1, 1)',
+        },
+      },
     },
   },
 };
