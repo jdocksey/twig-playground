f053dc5912b0ce3345ca19c0873a988ee35f3763
Joseph Docksey <joecode@tuta.io>
2023-08-30 12:23:41 -0500

    fix pin bouncing on mobile

--- input/tailwind.config.js
+++ input/tailwind.config.js
@@ -11,7 +11,8 @@ tailwind.config = {
     },
     animation: {
       // TODO: Add an explanation (if this works for mobile)
-      'bounce-by-growing': 'bounce-by-growing .8s infinite'
+      'bounce-by-growing': 'bounce-by-growing .8s infinite',
+      'bounce-by-growing-small': 'bounce-by-growing-small .8s infinite',
     },
     keyframes: {
       'bounce-by-growing': {
@@ -21,6 +22,16 @@ tailwind.config = {
         },
         '50%': {
           height: '4.8rem',
+          'animation-timing-function': 'cubic-bezier(0.95, 0, 1, 1)',
+        },
+      },
+      'bounce-by-growing-small': {
+        '0%, 100%': {
+          height: '2rem',
+          'animation-timing-function': 'cubic-bezier(0, 0, 0 .2, 1)',
+        },
+        '50%': {
+          height: '2.75rem',
           'animation-timing-function': 'cubic-bezier(0.95, 0, 1, 1)',
         },
       },
