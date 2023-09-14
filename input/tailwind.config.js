eb5c241d2c0427d94edd76658e699e4a4b9fe02a
Joseph Docksey <joecode@tuta.io>
2023-09-14 12:56:43 -0500

    colors

--- input/tailwind.config.js
+++ input/tailwind.config.js
@@ -10,5 +10,26 @@ tailwind.config = {
       'lg': '992px',
       'xl': '1201px',
     },
+    extend: {
+      // TODO: Move this out of `extend` when moving into the production code base
+      colors: {
+        black: '#000000',
+        blue: {
+          400: '#0abbe8',
+          800: '#0052a5',
+          950: '#001b3f',
+        },
+        cyan: {
+          50: "#e5f7fa",
+        },
+        gray: {
+          50: "#fafafa" /* background color */,
+        },
+        green: {
+          500: '#b6c42c',
+        },
+        white: '#ffffff',
+      },
+    },
   },
 };
