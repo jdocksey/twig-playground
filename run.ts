101c3ed4f1fa7cce2fb7e73fe0dfe658bc23c1f7
Joseph Docksey <joecode@tuta.io>
2023-10-04 13:43:02 -0500

    move icons

--- run.ts
+++ run.ts
@@ -82,6 +82,7 @@ function renderTemplate(template: string, data: any): string {
         data: template,
         namespaces: {
             macros: './input/macros',
+            icons: './input/partials/icons',
             globalPartials: './input/partials/global',
             localPartials: './input/partials/local',
         },
